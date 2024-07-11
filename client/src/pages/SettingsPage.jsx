73dbf5b7e758520d4a8a891174df37ee3c4f52de
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-07-11 17:32:54 -0400

    built the basic structure of the home and settings pages. added some js functionality to home and settings (linking btwn the two), producing a list from an array of objects, conditional rendering based on the click of the My Products button

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,6 +1,15 @@
+// NOTE TO MICHAEL:
+// If we decide that we don't want to fuss with the file upload handling and storage, we can allow users to simply add a link to an image URL
+
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import proPicSample from "../assets/images/profile-pic-sample.png";
 export default function SettingsPage() {
+  // created a testing function for the 'change' event of the file upload
+  function handleUpload(e) {
+    console.dir(e.target);
+    console.log(e.target.files);
+  }
   return (
     <>
       <header>
@@ -9,6 +18,7 @@ export default function SettingsPage() {
       <h1>Settings</h1>
       <div className="settingsDiv">
         <div>
+          {/* Below is the form to upload a new Profile Photo */}
           <form>
             <label htmlFor="profilePic">
               <h3>Profile Photo</h3>
@@ -19,9 +29,12 @@ export default function SettingsPage() {
                 alt="profile-pic"
                 src={proPicSample}
               ></img>
+              {/* The <div> below contains an input of type="file" */}
+              {/* This will require an "onChange" event listener */}
               <div className="proPicForm">
                 <label htmlFor="profilePicFile"></label>
                 <input
+                  onChange={handleUpload}
                   type="file"
                   id="profilePicFile"
                   name="profilePicFile"
@@ -44,33 +57,29 @@ export default function SettingsPage() {
           <h3>Email address</h3>
           <span>john.doe@gmail.com</span>
         </div>
-        <div>
-          <form>
+        <form>
+          <div>
             <label>
               <h3>Full Name</h3>
             </label>
             <input type="text" id="fullName" name="fullName"></input>
             <button>Edit</button>
-          </form>
-        </div>
-        <div>
-          <form>
+          </div>
+          <div>
             <label>
               <h3>Location</h3>
             </label>
             <input type="text" id="location" name="location"></input>
             <button>Edit</button>
-          </form>
-        </div>
-        <div>
-          <form>
+          </div>
+          <div>
             <label>
               <h3>Occupation</h3>
             </label>
             <input type="text" id="fullName" name="occupation"></input>
             <button>Edit</button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
       <footer>
         <Link to="/home">Home</Link>
