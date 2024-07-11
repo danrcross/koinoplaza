f448824798942e8f09c31132bba6058ec6ff9cf3
Ralph Molu <rmolu11@gmail.com>
2024-07-11 08:25:45 -0400

    server file generated, proxy added to vite.config

--- server/server.js
+++ server/server.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const db = require('./config/connection');
+const { ApolloServer } = require('@apollo/server');
+const { ExpressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('.utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+  if (process.env.NODE.ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    })
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+}
+
+startApolloServer();
