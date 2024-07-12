25d49261fc8851802f5eb5ecc5a84e7500d4f569
ajprobel <ajprobel@gmail.com>
2024-07-11 21:23:02 -0400

    add mutations to resolvers.js, update typeDefs, udpate seed.js data, add script

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,13 +6,13 @@ const resolvers = {
             return await Community.find({});
         },
         community: async (parent, { communityID }) => {
-            return await Community.findOne({ _id: communityID })
+            return await Community.findOne({ _id: communityID });
         },
         users: async () => {
             return await User.find({});
         },
         user: async (parent, { userID }) => {
-            return await User.findOne({ _id: userID })
+            return await User.findOne({ _id: userID });
         },
         products: async () => {
             return await Product.find({});
@@ -23,15 +23,75 @@ const resolvers = {
     },
 
     Mutation: {
-        addCommunity: async (parent, { info }) => {
-            return Community.create({ info })
+        // create a new community
+        addCommunity: async (parent, args) => {
+            return await Community.create(args).then((result) => console.log(result))
         },
-        addUser: async (parent, { info }) => {
-            return User.create({ info })
+
+        // create a new user
+        addUser: async (parent, args) => {
+            return await User.create(args)
         },
-        addProduct: async (parent, { info }) => {
-            return Product.create({ info })
-        }
+        
+        // Create a new product. This will take the user's input for name/desc/price, as well desired community
+        // first it creates a new Product, then adds that product ID to the Community and the User
+        addProduct: async (parent, args) => {
+            const { name, description, price, userID, communityID } = args
+            // creates the new product
+            const newProduct = new Product({ name, description, price });
+            await newProduct.save();
+            // saves that new product's ID to be used for updating database
+            const newProductID = newProduct._id;
+            await Product.findOneAndUpdate(
+                { _id: newProductID },
+                { $addToSet: { createdBy: userID, community: communityID} },
+                { new: true }
+            );
+            await User.findOneAndUpdate(
+                { _id: userID },
+                { $addToSet: { products: newProductID } },
+                { new: true }
+            );
+            await Community.findOneAndUpdate(
+                { _id: communityID },
+                { $addToSet: { products: newProductID } },
+                { new: true }
+            );
+            return;
+        },
+
+        // Remove a product from user's array first, then remove the occurence
+        removeProduct: async (parent, { productID }) => {
+            await User.findOneAndUpdate(
+                { products: productID },
+                { $pull: { products: productID } }
+            );
+            return await Product.findOneAndDelete({ _id:productID });
+        },
+
+        joinCommunity: async (parent, { userID, communityID }) => {
+            await Community.findOneAndUpdate(
+                { _id: communityID },
+                { $addToSet: { users: userID } },
+            );
+            return await User.findOneAndUpdate(
+                { _id: userID },
+                { $addToSet: { communities: communityID } },
+                { new: true }
+            );
+        },
+
+        leaveCommunity: async (parent, { userID, communityID }) => {
+            await Community.findOneAndUpdate(
+                { _id: communityID },
+                { $pull: { users: userID } },
+            )
+            return await User.findOneAndUpdate(
+                {_id: userID},
+                { $pull: { communities: communityID} },
+                { new: true }
+            )
+        },
     }
 };
 
