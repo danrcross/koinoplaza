b11a296c197596b19fba3e92da5ce2da147cb357
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-07-13 16:58:09 -0400

    added basic linking functionality to places where lists are populated with communities or goods

--- client/src/components/CommunityProducts/index.jsx
+++ client/src/components/CommunityProducts/index.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 function CommunityProducts({ commProductData }) {
   return (
     <div className="communityProductsList">
       {commProductData.map((item) => (
         <div key={item.id}>
-          <div>
-            <h4>{item.product}</h4>
-            <ul>
-              <li>Price: ${item.price}</li>
-              <li>Condition: {item.condition}</li>
-              <li>
-                Seller: {item.seller.name} {`(${item.seller.rating})`}
-              </li>
-            </ul>
-          </div>
+          <Link to={`/products/${item.id}`}>
+            <div>
+              <h4>{item.product}</h4>
+              <ul>
+                <li>Price: ${item.price}</li>
+                <li>Condition: {item.condition}</li>
+                <li>
+                  Seller: {item.seller.name} {`(${item.seller.rating})`}
+                </li>
+              </ul>
+            </div>
+          </Link>
         </div>
       ))}
     </div>
--- client/src/components/MyProducts/index.jsx
+++ client/src/components/MyProducts/index.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 function MyProducts({ myProductData }) {
   return (
     <div className="myProductsList">
       {myProductData.map((item) => (
         <div key={item.id}>
-          <div>
-            <h4>{item.product}</h4>
-            <ul>
-              <li>Price: ${item.price}</li>
-              <li>Condition: {item.condition}</li>
-              <li>
-                Seller: {item.seller.name} {`(${item.seller.rating})`}
-              </li>
-            </ul>
-          </div>
+          <Link to={`/products/${item.id}`}>
+            <div>
+              <h4>{item.product}</h4>
+              <ul>
+                <li>Price: ${item.price}</li>
+                <li>Condition: {item.condition}</li>
+                <li>
+                  Seller: {item.seller.name} {`(${item.seller.rating})`}
+                </li>
+              </ul>
+            </div>
+          </Link>
         </div>
       ))}
     </div>
