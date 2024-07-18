b41d633fe84878e83816938d620debf935d608aa
ajprobel <ajprobel@gmail.com>
2024-07-17 22:47:49 -0400

    add JS logic to NewCommunityPage.jsx, update client mutations.js and server resolvers.js

--- client/src/pages/NewCommunityPage.jsx
+++ client/src/pages/NewCommunityPage.jsx
@@ -1,8 +1,72 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useNavigate } from "react-router-dom";
+
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_CURRENT_USER } from "../utils/queries";
+import { CREATE_COMMUNITY } from "../utils/mutations";
 
 import sampPic from "../assets/images/wvlok.png";
 export default function NewCommunityPage() {
+  const navigate = useNavigate();
+
+  // use store the data for the form in a state
+  const [formState, setFormState] = useState({
+    name: "",
+    description: "",
+    location: "",
+    image: "",
+  });
+
+  // Define our Query and Mutation
+  const { data, loading } = useQuery(GET_CURRENT_USER);
+  const [createCommunity] = useMutation(CREATE_COMMUNITY);
+
+  // Handles Form Change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+
+    // for later, if we want to notify users that fields can't be empty while filling out form
+    if ((name === "name" || "location" || "description") && value === "") {
+      console.log("field cannot be blank!");
+    }
+  };
+
+  // Upon clicking "Create Community"...
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    // get the current user._id, as well as the values in the form
+    const userID = (await data?.currentUser._id) || [];
+    const { name, description, location, image } = formState;
+
+    // perform mutation with those values. Upon success, redirect to the users communities
+    try {
+      await createCommunity({
+        variables: {
+          name,
+          description,
+          location,
+          image,
+          createdBy: userID,
+        },
+      });
+      window.alert(
+        `New Community, ${name}, successfully created! \n Redirecting to your communities`
+      );
+      navigate("/communities");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="newItemPage">
       <Header />
@@ -20,8 +84,10 @@ export default function NewCommunityPage() {
               <input
                 type="url"
                 id="ncPicUrl"
-                name="ncPicUrl"
+                name="image"
                 className="itemInputShort"
+                value={formState.image}
+                onChange={handleChange}
               ></input>
             </div>
           </div>
@@ -31,7 +97,9 @@ export default function NewCommunityPage() {
               className="itemInputLong"
               type="text"
               id="commName"
-              name="commName"
+              name="name"
+              value={formState.name}
+              onChange={handleChange}
             ></input>
           </div>
 
@@ -42,9 +110,25 @@ export default function NewCommunityPage() {
               type="text"
               id="location"
               name="location"
+              value={formState.location}
+              onChange={handleChange}
             ></input>
           </div>
-          <input className="submitBtn" type="submit" value="Create Community" />
+
+          <label className="newItemLabel">Description</label>
+          <div className="inputWrapper">
+            <input
+              className="itemInputLong"
+              type="text"
+              id="description"
+              name="description"
+              value={formState.description}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <button className="submitBtn" onClick={handleClick}>
+            Create Community
+          </button>
         </form>
       </div>
       <Footer />
